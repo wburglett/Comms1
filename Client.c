166fe65b7772036e056638f7e0de74c23286bd83
Aaron Burger <wburglett@Executor-Mobile.(none)>
2013-10-02 21:59:36 -0400

    Completed client and server

--- Client.c
+++ Client.c
@@ -1,3 +1,17 @@
+/*
+Aaron Burger
+October 2, 2013
+Computer Networks
+Columbia University
+
+Client.c
+
+This source describes the web client we have been
+requested to create via class specifications. It
+can perform the 'credentials' command to grab credential
+payload from the server.
+*/
+
 #include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -7,34 +21,36 @@
 #define STDIN 0
 
 int main (int argc, char **argv) {
-	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stdout, NULL, _IONBF, 0); // printf flushing control
 	
-	int i=0;
+	// Constant and variable initilization
+
+	int i=0; // Iterators
 	int status = 0;
 	int control = -1;
 	int breakout = 1;
-	int lport = 34789;
+	int lport = 34789; // Port defaults
 	int tries = 0;
 
-	long long laddr = inet_addr("127.0.0.1");
+	long long laddr = inet_addr("127.0.0.1"); // Default IP address
 
-	char welcome[1] = "0";
+	char welcome[1] = "0"; // Reference constant initilization
 	char incorrect[1] = "1";
 	char lock[1] = "2";
 	char input[1] = "3";
 	char broad[1] = "4";
 	char disc[1] = "5";
 	
-	char auth[100];
+	char auth[100]; // Large array initilization
 	char indata[1000];
 	char clin[100];
 
-	fd_set servers;
+	fd_set servers; // Socked list initilization
 	fd_set backup;
 	FD_ZERO(&servers);
 	FD_ZERO(&backup);
 
-	memset(auth, '\0', sizeof(auth[0]) * 100);
+	memset(auth, '\0', sizeof(auth[0]) * 100); // Memory setup for large arrays
 	memset(indata, '\0', sizeof(indata[0]) * 100);
 	memset(clin, '\0', sizeof(clin[0]) * 100);
 
@@ -43,7 +59,7 @@ int main (int argc, char **argv) {
 		lport = atoi(argv[2]);
 	}
 
-	struct sockaddr_in saddr;
+	struct sockaddr_in saddr; // Socket structure initialization
 	struct sockaddr *uaddr = (struct sockaddr *) &saddr;
 	saddr.sin_family = AF_INET;
 	saddr.sin_port = lport;
@@ -52,7 +68,7 @@ int main (int argc, char **argv) {
 
 	int s = socket(PF_INET, SOCK_STREAM, 0);
 	
-	FD_SET(STDIN, &servers);
+	FD_SET(STDIN, &servers); // Add default socket and STDIN to list
 	FD_SET(s, &servers);
 	FD_SET(STDIN, &backup);
 	FD_SET(s, &backup);
@@ -64,38 +80,38 @@ int main (int argc, char **argv) {
 		perror("Connection Failed");
 	}
 
-	while (breakout == 1) {
-		if (status = recv(s, &indata, sizeof(indata), 0) <= 0) {
+	while (breakout == 1) { // Main control loop
+		if (status = recv(s, &indata, sizeof(indata), 0) <= 0) { // If a receive error occurs, report it but don't quit
 			perror ("On data receive");
 		}
 
-		if (indata[0] == welcome[0]) {
+		if (indata[0] == welcome[0]) { // If the server is ready for authentication
 			memset(auth, '\0', sizeof(auth[0]) * 100);
 			auth[0] = auth[1] = '0';
 
-			printf("Username: ");
+			printf("Username: "); // Present username
 			read(0, clin, sizeof(clin));
 			clin[strcspn(clin, "\n")] = '\0';
 			strcat(auth, clin);
 			strcat(auth, " ");
 
-			printf("Password: ");
+			printf("Password: "); // Present password
 			read(0, clin, sizeof(clin));
 			clin[strcspn(clin, "\n")] = '\0';
-			strcat(auth, clin);
+			strcat(auth, clin); // Merge the two into an auth token string
 
 			if (status = send(s, &auth, sizeof(auth), 0) <= 0) {
 				perror ("On auth send");
 			}
 			tries++;
 		}
-		else if (indata[0] == incorrect[0]) {
+		else if (indata[0] == incorrect[0]) { // If the server says the credentials are invalid
 			memset(auth, '\0', sizeof(auth[0]) * 100);
 			printf("\nBad credentials. Please try again\n");
 			auth[0] = '0';
 			auth[1] = 48 + tries;
 
-			printf("Username: ");
+			printf("Username: "); // Get new credentials and try again
 			read(0, clin, sizeof(clin));
 			clin[strcspn(clin, "\n")] = '\0';
 			strcat(auth, clin);
@@ -111,7 +127,7 @@ int main (int argc, char **argv) {
 			}
 			tries++;
 		}
-		else if (indata[0] == lock[0]) {
+		else if (indata[0] == lock[0]) { // Do the same if the server is locked, but tell users to wait one minute
 			memset(auth, '\0', sizeof(auth[0]) * 100);
 			printf("\nLogin locked. Please wait one minute before trying again\n");
 			tries = 0;
@@ -135,32 +151,32 @@ int main (int argc, char **argv) {
 			}
 			tries++;
 		}
-		else if (indata[0] == input[0]) {
+		else if (indata[0] == input[0]) { // If the server is ready for command input
 			if (tries > 0) {
-				printf("\nLogin Successful! Welcome to the Burger Server\n");
+				printf("\nLogin Successful! Welcome to the Burger Server\n"); // Present welcome message
 				tries = 0;
 			}
 			printf("Input command: ");
 
-			while (breakout == 1) {
+			while (breakout == 1) { // Enter command input stage
 				servers = backup;
 				if (select(s+1, &servers, NULL, NULL, NULL) == -1) {
 					perror("Select Failed");
 					breakout = 0;
 				}
-				for (i=0; i<=s; i++) {
+				for (i=0; i<=s; i++) { // For the two relavent sockets (server connection and STDIN)
 					if (FD_ISSET(i, &servers)) {
-						if (i == STDIN) {
+						if (i == STDIN) { // If there is user input
 							if (read(i, clin, sizeof(clin)) > 0) {
 								clin[strcspn(clin, "\n")] = '\0';
 								
-								if (!strcmp(clin, "credentials")) {
+								if (!strcmp(clin, "credentials")) { // Credentials asks for the credentials payload
 									if (status = send(s, &input, sizeof(input), 0) <= 0) {
 										perror ("On cred request send");
 									}
 									memset(clin, '\0', sizeof(clin[0]) * 100);
 								}
-								else if (!strcmp(clin, "quit")) {
+								else if (!strcmp(clin, "quit")) { // Quit disconnects from the server
 									breakout = 0;
 									strcat(disc, auth);
 
@@ -168,21 +184,21 @@ int main (int argc, char **argv) {
 										perror ("On disc send");
 									}
 								}
-								else {
+								else { // All other commands are invalid
 									printf("%s is not a valid command\n\nInput command: ", clin);
 								}
 							}
 						}
-						else {
-							if (status = recv(i, indata, sizeof(indata), 0) <= 0) {
+						else { // If the server is sending a message
+							if (status = recv(i, indata, sizeof(indata), 0) <= 0) { // It could be offline, so disconnect
 								perror ("Forced disconnect");
 								breakout = 0;
 							}
-							else if (indata[0] == broad[0]) {
+							else if (indata[0] == broad[0]) { // It could be presenting a broadcast, print it
 								printf("-- SERVER BROADCAST: %s --\n", indata+1);
 								printf("Input command: ");
 							}
-							else if (indata[0] == input[0]) {
+							else if (indata[0] == input[0]) { // It could be presenting authentications, print them
 								printf("\nUsername - Password Combinations\n%s\n", indata+1);
 								printf("Input command: ");
 							}
@@ -191,13 +207,13 @@ int main (int argc, char **argv) {
 				}
 			}
 		}
-		else {
+		else { // If the server is force quit at some points in operation, this may happen. Forces disconnect
 			printf("The server has garbled a command\n");
 			breakout = 0;
 		}
 	}
 
-	if ((status = close(s)) == 0) {
+	if ((status = close(s)) == 0) { // Close socket
 		printf("Connection Closed\n");
 	}
 	else {
--- Server.c
+++ Server.c
@@ -1,3 +1,24 @@
+/*
+Aaron Burger
+October 2, 2013
+Computer Networks
+Columbia University
+
+Server.c
+
+This source describes the web server we have been
+requested to create via class specifications. It
+can perform the whoelse and wholasthr commands to
+search for relevent clients, and the broadcast
+command to send a message to clients that are currently connected
+
+The order of the loops and help with list control was based in part
+off an online tutorial that can be found here:
+http://beej.us/guide/bgnet/output/html/singlepage/bgnet.html
+All content and operation of the server are original.
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,60 +32,68 @@
 #define USERS 9
 
 int main (int argc, char **argv) {
-	signal(SIGPIPE, SIG_IGN);
-	setvbuf(stdout, NULL, _IONBF, 0);
+	signal(SIGPIPE, SIG_IGN); // supression prevention control
+	setvbuf(stdout, NULL, _IONBF, 0); // printf flushing control
 
-	int i = 0;
+	// Constant and variable initilization
+
+	int i = 0; // Iterators
 	int j = 0;
-	int cllen = 0;
+	int inx = 0;
+	int cllen = 0; // Socket data lengths
 	int dalen = 0;
 	int inlen = 0;
-	int breakout = 1;
-	int present = 0;	
+	int breakout = 1; // Control flow
+	int present = 0;
+	int prevent = 0;	
 	int status = 0;	
-	int lport = 34789;
+	int lport = 34789; // Port information
 	int clientmax = 0;
 
-	char *search = " ";
+	char *search = " "; // String pointer initiaization
 	char *end = "\0";
 	char *user;
 	char *pass;
 
-	char welcome[1] = "0";
+	char welcome[1] = "0"; // Reference constant initilization
 	char incorrect[1] = "1";
 	char lock[1] = "2";
 	char input[1] = "3";
 	char broad[1] = "4";
 	char disc[1] = "5";
 
-	char cldata[100];
+	char cldata[100]; // Large array initilization
 	char bmsg[100];
 	char *allauths = malloc(1000*sizeof(char));
 	char *auth[USERS][2];
 	long long timestamps[USERS][3];
+	long long iplocks[USERS][2];
 	
-	struct sockaddr_in saddr;
+	struct sockaddr_in saddr; // Socket structure initialization
 	struct sockaddr *uaddr = (struct sockaddr *) &saddr;
+	struct sockaddr_in claddrin;
 	struct sockaddr_storage claddr;
 	struct sockaddr *ucladdr = (struct sockaddr *) &claddr;
+	struct in_addr addr;
 
-	fd_set lisocks;
+	fd_set lisocks; // Socket list initilization
 	fd_set clients;
 	FD_ZERO(&lisocks);
 	FD_ZERO(&clients);
 
-	memset(bmsg, '\0', sizeof(cldata[0]) * 100);
+	memset(bmsg, '\0', sizeof(cldata[0]) * 100); // Memory setup for large arrays
 	memset(cldata, '\0', sizeof(cldata[0]) * 100);
 	memset(timestamps, 0, sizeof(timestamps[0][0]) * USERS * 3);
+	memset(iplocks, 0, sizeof(iplocks[0][0]) * USERS * 2);
 
 	for (i=0; i<USERS; i++)
 		for (j=0; j<2; j++)
 			auth[i][j] = malloc(100*sizeof(char));
 
-	strcpy(allauths, "3");
+	strcpy(allauths, "3"); // Hard coded initilizations
 	bmsg[0] = broad[0];
 
-	FILE* myfile=NULL;
+	FILE* myfile=NULL; // Read login credentials from file
 	myfile=fopen("Credentials.txt","r");
 	for(i=0; i<USERS; i++) {
 		fscanf(myfile, "%[^ \n] %[^ \n]\n", auth[i][0], auth[i][1]);
@@ -75,11 +104,11 @@ int main (int argc, char **argv) {
 	}
 	fclose(myfile);
 
-	if (argc == 2) {
+	if (argc == 2) { // Read port as an argument from command line, if desired
 		lport = atoi(argv[1]);
 	}
 
-	saddr.sin_family = AF_INET;
+	saddr.sin_family = AF_INET; // Socket initialization
 	saddr.sin_port = lport;
 	saddr.sin_addr.s_addr = INADDR_ANY;
 	int addrsize = sizeof(saddr);
@@ -87,41 +116,41 @@ int main (int argc, char **argv) {
 	int s = socket(PF_INET, SOCK_STREAM, 0);
 	int clsock;
 	
-	if ((status = bind(s, uaddr, addrsize)) == 0) {
+	if ((status = bind(s, uaddr, addrsize)) == 0) { // Binding step
 		printf("Socket Bound\n");
 	}
 	else {
 		perror("Bind Failed\n");
 	}
-	if ((status = listen(s, 10)) == 0) {
+	if ((status = listen(s, 10)) == 0) { // Listening step
 		printf("Liste‌ning\n\n");
 	}
 	else {
 		perror("Listen Failed\n");
 	}
 
-	FD_SET(STDIN, &lisocks);
+	FD_SET(STDIN, &lisocks); // Add default socket and STDIN to list
 	FD_SET(s, &lisocks);
 
 	clientmax = s;
 
 	printf("Please enter a command: ");
 
-	while (breakout == 1) {
-		clients = lisocks;
-		if (select(clientmax+1, &clients, NULL, NULL, NULL) == -1) {
+	while (breakout == 1) { // Main control loop
+		clients = lisocks; // Load in all sockets to the selector
+		if (select(clientmax+1, &clients, NULL, NULL, NULL) == -1) { // Select step
 			perror("Select Failed");
 		}
 
-		for (i=0; i<=clientmax; i++) {			
-			if (FD_ISSET(i, &clients)) {
-				if (i == s) {
+		for (i=0; i<=clientmax; i++) { // For all possible sockets	
+			if (FD_ISSET(i, &clients)) { // If the socket is writing
+				if (i == s) { // If the default socket is being written to, there is a new client
 					cllen = sizeof(claddr);
 					clsock = accept(s, ucladdr, &cllen);
 					if (clsock == -1) {
 						perror("Couldn't Accept");
 					}
-					else {
+					else { // Add new client to relevent lists
 						FD_SET(clsock, &lisocks);
 						if (clsock > clientmax) clientmax = clsock;
 						if ((status = send(clsock, &welcome, sizeof(welcome), 0)) <= 0) {
@@ -129,44 +158,47 @@ int main (int argc, char **argv) {
 						}
 					}
 				}
-				else if (i == STDIN) {
+				else if (i == STDIN) { // If the socket is STDIN, take input from user
 					if ((dalen = read(i, cldata, 100*sizeof(char))) > 0) {
 						cldata[strlen(cldata)-1] = '\0';
-						if (!strcmp(cldata, "whoelse")) {
-							for (j=0; j<USERS; j++) {		
+						if (!strcmp(cldata, "whoelse")) { // Whoelse command shows currently connected users
+							for (j=0; j<USERS; j++) {
+       		 						addr.s_addr = timestamps[j][2];
 								if (timestamps[j][0] != 0) {
-									printf("%s, ", auth[j][0]);
+									printf("%s - %lld - %s\n", auth[j][0], timestamps[j][1], inet_ntoa(addr));
 									present = 1;
 								}
 							}
+							printf("\n");
 							if (present) {
-								printf("are here\n\n");
 								present = 0;
 							}
 							else {
 								printf("No users are here\n\n");
 							} 
 						}
-						else if (!strcmp(cldata, "wholasthr")) {
-							for (j=0; j<USERS; j++) {		
+						else if (!strcmp(cldata, "wholasthr")) { // Wholasthr does that for the last hour
+							for (j=0; j<USERS; j++) {
+       		 						addr.s_addr = timestamps[j][2];
+	
 								if (timestamps[j][1] > time(NULL) - 3600 || timestamps[j][0] != 0) {
-									printf("%s, ", auth[j][0]);
+									printf("%s - %lld - %s\n", auth[j][0], timestamps[j][1], inet_ntoa(addr));
 									present = 1;
 								}
 							}
+							printf("\n");
 							if (present) {
-								printf("were here in the last hour\n\n");
 								present = 0;
 							}
 							else {
-								printf("No users are here\n\n");
+								printf("No users were here\n\n");
 							}
 						}
-						else if (!strcmp(cldata, "quit")) {
+						else if (!strcmp(cldata, "quit")) { // Quit breaks out of the control loop
 							breakout = 0;
 						}
 						else if (strcmp(cldata, "broadcast") != 0 && !strcmp(strtok(cldata, search), "broadcast")) {
-							strcat(bmsg, strtok(NULL, end));
+							strcat(bmsg, strtok(NULL, end)); // The broadcast string is formed to be sent to all clients
 
 							for (j=0; j<=clientmax; j++) {			
 								if (FD_ISSET(j, &lisocks)) {
@@ -181,23 +213,32 @@ int main (int argc, char **argv) {
 							memset(bmsg, '\0', sizeof(bmsg[0]) * 100);
 							bmsg[0] = broad[0];
 						}
-						else if (!strcmp(cldata, "broadcast") != 0) {
+						else if (!strcmp(cldata, "broadcast") != 0) { // Broadcast error control
 							printf("Must broadcast some message\n\n");
 						}
-						else {
+						else { // Step for invalid commands
 							printf("%s is not a valid command\n\n", cldata);
 						}
-						if (breakout) printf("Please enter a command: ");
+						if (breakout) printf("Please enter a command: "); // Don't prompt if no more commands
 						memset(cldata, '\0', 100);
 					}
 				}
-				else {
-					if ((status = recv(i, cldata, sizeof(cldata), 0)) <= 0) {
+				else { // If a client is writing to the server on its socket
+					if ((status = recv(i, cldata, sizeof(cldata), 0)) <= 0) { // If there is no data, discontinue the client
 						close(i);
                         			FD_CLR(i, &lisocks);
 					}
-					else {
-						if (cldata[0] == welcome[0]) {
+					else { // If there is data, interpret it
+						prevent = 0;
+						getsockname(i, (struct sockaddr *)&claddrin, &cllen);
+						for (j=0; j<inx; j++) {
+							if (inet_addr(inet_ntoa(claddrin.sin_addr)) == iplocks[j][0] && time(NULL) < iplocks[j][1]) {
+								prevent = 1;
+								break;
+							}
+						}
+
+						if (cldata[0] == welcome[0] && prevent == 0) { // If it is login data, verify credentials. Can't be a blocked IP
 							user = strtok(cldata, search)+2;
 							pass = strtok(NULL, search);
 
@@ -205,7 +246,7 @@ int main (int argc, char **argv) {
 								if (!strcmp(auth[j][0], user) && !strcmp(auth[j][1], pass)) {
 									timestamps[j][0] += 1;
 									timestamps[j][1] = time(NULL);
-									timestamps[j][2] = time(NULL);
+									timestamps[j][2] = inet_addr(inet_ntoa(claddrin.sin_addr));
 									present = 1;
 
 									if ((status = send(i, &input, sizeof(input), 0)) <= 0) {
@@ -215,13 +256,16 @@ int main (int argc, char **argv) {
 								}
 							}
 
-							if (present == 0) {
-								if (cldata[1] >= '2') {
+							if (present == 0) { // If login unsuccessful
+								if (cldata[1] >= '2') { // Three attempts means IP Block
 									if ((status = send(i, &lock, sizeof(lock), 0)) <= 0) {
 										perror("Send");
 									}
+									iplocks[inx][0] = inet_addr(inet_ntoa(claddrin.sin_addr));
+									iplocks[inx][1] = time(NULL) + 60;
+									inx++;
 								}
-								else {
+								else { // Ask for credentials again otherwise
 									if ((status = send(i, &incorrect, sizeof(incorrect), 0)) <= 0) {
 										perror("Send");
 									}
@@ -231,12 +275,17 @@ int main (int argc, char **argv) {
 								present = 0;
 							}
 						}
-						else if (cldata[0] == input[0]) {
+						else if (prevent == 1) { // Send a lock message if locked, don't lock again
+							if ((status = send(i, &lock, sizeof(lock), 0)) <= 0) {
+								perror("Send");
+							}
+						}
+						else if (cldata[0] == input[0]) { // If the authentication payload is requested, send it
 							if ((status = send(i, allauths, 1000*sizeof(char), 0)) <= 0) {
 								perror("Send");
 							}
 						}
-						else if (cldata[0] == disc[0]) {
+						else if (cldata[0] == disc[0]) { // If a disconnect is sent, remove the client
 							user = strtok(cldata, search)+3;
 							pass = strtok(NULL, search);
 							for (j=0; j<USERS; j++) {
@@ -253,7 +302,7 @@ int main (int argc, char **argv) {
 		}
 	}
 
-	if ((status = close(s)) == 0) {
+	if ((status = close(s)) == 0) { // Close the socket
 		printf("Socket Closed\n");
 	}
 	else {
