a1092a2f2d17bd6816f784e27f4258c9f11b358e
Aaron Burger <wburglett@Executor-Mobile.(none)>
2013-10-02 20:34:24 -0400

    Lots of new features

--- Client.c
+++ Client.c
@@ -9,28 +9,38 @@
 int main (int argc, char **argv) {
 	setvbuf(stdout, NULL, _IONBF, 0);
 	
+	int i=0;
 	int status = 0;
 	int control = -1;
 	int breakout = 1;
 	int lport = 34789;
 	int tries = 0;
 
+	long long laddr = inet_addr("127.0.0.1");
+
 	char welcome[1] = "0";
 	char incorrect[1] = "1";
 	char lock[1] = "2";
 	char input[1] = "3";
-	char broad[1] = "5";
+	char broad[1] = "4";
+	char disc[1] = "5";
 	
 	char auth[100];
-	char indata[100];
+	char indata[1000];
 	char clin[100];
+
+	fd_set servers;
+	fd_set backup;
+	FD_ZERO(&servers);
+	FD_ZERO(&backup);
 
 	memset(auth, '\0', sizeof(auth[0]) * 100);
 	memset(indata, '\0', sizeof(indata[0]) * 100);
 	memset(clin, '\0', sizeof(clin[0]) * 100);
 
-	if (argc == 2) {
-		lport = atoi(argv[1]);
+	if (argc == 3) {
+		laddr = inet_addr(argv[1]);
+		lport = atoi(argv[2]);
 	}
 
 	struct sockaddr_in saddr;
@@ -42,8 +52,13 @@ int main (int argc, char **argv) {
 
 	int s = socket(PF_INET, SOCK_STREAM, 0);
 	
+	FD_SET(STDIN, &servers);
+	FD_SET(s, &servers);
+	FD_SET(STDIN, &backup);
+	FD_SET(s, &backup);
+
 	if ((status = connect(s, uaddr, addrsize)) == 0) {
-		printf("Connection Made\n");
+		printf("Connection established, please log in\n");
 	}
 	else {
 		perror("Connection Failed");
@@ -55,6 +70,7 @@ int main (int argc, char **argv) {
 		}
 
 		if (indata[0] == welcome[0]) {
+			memset(auth, '\0', sizeof(auth[0]) * 100);
 			auth[0] = auth[1] = '0';
 
 			printf("Username: ");
@@ -72,10 +88,10 @@ int main (int argc, char **argv) {
 				perror ("On auth send");
 			}
 			tries++;
-			memset(auth, '\0', sizeof(auth[0]) * 100);
 		}
 		else if (indata[0] == incorrect[0]) {
-			printf("Bad Login\n");
+			memset(auth, '\0', sizeof(auth[0]) * 100);
+			printf("\nBad credentials. Please try again\n");
 			auth[0] = '0';
 			auth[1] = 48 + tries;
 
@@ -94,10 +110,10 @@ int main (int argc, char **argv) {
 				perror ("On auth send");
 			}
 			tries++;
-			memset(auth, '\0', sizeof(auth[0]) * 100);
 		}
 		else if (indata[0] == lock[0]) {
-			printf("Login Locked\n");
+			memset(auth, '\0', sizeof(auth[0]) * 100);
+			printf("\nLogin locked. Please wait one minute before trying again\n");
 			tries = 0;
 
 			auth[0] = '0';
@@ -118,25 +134,71 @@ int main (int argc, char **argv) {
 				perror ("On auth send");
 			}
 			tries++;
-			memset(auth, '\0', sizeof(auth[0]) * 100);
 		}
 		else if (indata[0] == input[0]) {
-			printf("Data input\n");
-		}
-		else if (indata[0] == broad[0]) {
-			printf("Receiving data\n");
+			if (tries > 0) {
+				printf("\nLogin Successful! Welcome to the Burger Server\n");
+				tries = 0;
+			}
+			printf("Input command: ");
+
+			while (breakout == 1) {
+				servers = backup;
+				if (select(s+1, &servers, NULL, NULL, NULL) == -1) {
+					perror("Select Failed");
+					breakout = 0;
+				}
+				for (i=0; i<=s; i++) {
+					if (FD_ISSET(i, &servers)) {
+						if (i == STDIN) {
+							if (read(i, clin, sizeof(clin)) > 0) {
+								clin[strcspn(clin, "\n")] = '\0';
+								
+								if (!strcmp(clin, "credentials")) {
+									if (status = send(s, &input, sizeof(input), 0) <= 0) {
+										perror ("On cred request send");
+									}
+									memset(clin, '\0', sizeof(clin[0]) * 100);
+								}
+								else if (!strcmp(clin, "quit")) {
+									breakout = 0;
+									strcat(disc, auth);
+
+									if (status = send(s, &disc, 100*sizeof(char), 0) <= 0) {
+										perror ("On disc send");
+									}
+								}
+								else {
+									printf("%s is not a valid command\n\nInput command: ", clin);
+								}
+							}
+						}
+						else {
+							if (status = recv(i, indata, sizeof(indata), 0) <= 0) {
+								perror ("Forced disconnect");
+								breakout = 0;
+							}
+							else if (indata[0] == broad[0]) {
+								printf("-- SERVER BROADCAST: %s --\n", indata+1);
+								printf("Input command: ");
+							}
+							else if (indata[0] == input[0]) {
+								printf("\nUsername - Password Combinations\n%s\n", indata+1);
+								printf("Input command: ");
+							}
+						}
+					}
+				}
+			}
 		}
 		else {
 			printf("The server has garbled a command\n");
 			breakout = 0;
 		}
-
-		perror ("Recv");
-		printf("%d %d\n", status, indata[0]);
 	}
 
 	if ((status = close(s)) == 0) {
-		printf("Socket Closed\n");
+		printf("Connection Closed\n");
 	}
 	else {
 		perror("Close Failed");
--- Server.c
+++ Server.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -25,6 +26,7 @@ int main (int argc, char **argv) {
 	int clientmax = 0;
 
 	char *search = " ";
+	char *end = "\0";
 	char *user;
 	char *pass;
 
@@ -32,28 +34,15 @@ int main (int argc, char **argv) {
 	char incorrect[1] = "1";
 	char lock[1] = "2";
 	char input[1] = "3";
-	char broad[1] = "5";
+	char broad[1] = "4";
+	char disc[1] = "5";
 
 	char cldata[100];
+	char bmsg[100];
+	char *allauths = malloc(1000*sizeof(char));
 	char *auth[USERS][2];
-	long long timestamps[USERS][2];
+	long long timestamps[USERS][3];
 	
-	memset(cldata, '\0', sizeof(cldata[0]) * 100);
-	memset(auth, '\0', sizeof(auth[0][0]) * 10 * 2);
-	memset(timestamps, 0, sizeof(timestamps[0][0]) * 10 * 2);
-
-	char temp1[100];
-	char temp2[100];
-
-	FILE* myfile=NULL;
-	myfile=fopen("Credentials.txt","r");
-	for(i=0; i<USERS; i++) {
-		fscanf(myfile, "%[^ \n] %[^ \n]\n", temp1, temp2);
-		auth[i][0] = temp1;
-		auth[i][1] = temp2;
-	}
-	fclose(myfile);
-
 	struct sockaddr_in saddr;
 	struct sockaddr *uaddr = (struct sockaddr *) &saddr;
 	struct sockaddr_storage claddr;
@@ -63,7 +52,29 @@ int main (int argc, char **argv) {
 	fd_set clients;
 	FD_ZERO(&lisocks);
 	FD_ZERO(&clients);
-	
+
+	memset(bmsg, '\0', sizeof(cldata[0]) * 100);
+	memset(cldata, '\0', sizeof(cldata[0]) * 100);
+	memset(timestamps, 0, sizeof(timestamps[0][0]) * USERS * 3);
+
+	for (i=0; i<USERS; i++)
+		for (j=0; j<2; j++)
+			auth[i][j] = malloc(100*sizeof(char));
+
+	strcpy(allauths, "3");
+	bmsg[0] = broad[0];
+
+	FILE* myfile=NULL;
+	myfile=fopen("Credentials.txt","r");
+	for(i=0; i<USERS; i++) {
+		fscanf(myfile, "%[^ \n] %[^ \n]\n", auth[i][0], auth[i][1]);
+		strcat(allauths, auth[i][0]);
+		strcat(allauths, "-");
+		strcat(allauths, auth[i][1]);
+		strcat(allauths, "\n");
+	}
+	fclose(myfile);
+
 	if (argc == 2) {
 		lport = atoi(argv[1]);
 	}
@@ -104,7 +115,7 @@ int main (int argc, char **argv) {
 
 		for (i=0; i<=clientmax; i++) {			
 			if (FD_ISSET(i, &clients)) {
-				if (i == s) { // Listening socket is selected
+				if (i == s) {
 					cllen = sizeof(claddr);
 					clsock = accept(s, ucladdr, &cllen);
 					if (clsock == -1) {
@@ -119,11 +130,11 @@ int main (int argc, char **argv) {
 					}
 				}
 				else if (i == STDIN) {
-					if ((dalen = read(i, cldata, sizeof(cldata))) > 0) {
+					if ((dalen = read(i, cldata, 100*sizeof(char))) > 0) {
 						cldata[strlen(cldata)-1] = '\0';
 						if (!strcmp(cldata, "whoelse")) {
 							for (j=0; j<USERS; j++) {		
-								if (timestamps[j][0] == 1) {
+								if (timestamps[j][0] != 0) {
 									printf("%s, ", auth[j][0]);
 									present = 1;
 								}
@@ -138,7 +149,7 @@ int main (int argc, char **argv) {
 						}
 						else if (!strcmp(cldata, "wholasthr")) {
 							for (j=0; j<USERS; j++) {		
-								if (timestamps[j][1] > time(NULL) - 3600 || timestamps[j][0] == 1) {
+								if (timestamps[j][1] > time(NULL) - 3600 || timestamps[j][0] != 0) {
 									printf("%s, ", auth[j][0]);
 									present = 1;
 								}
@@ -151,55 +162,88 @@ int main (int argc, char **argv) {
 								printf("No users are here\n\n");
 							}
 						}
-						else if (!strcmp(cldata, "broadcast")) {
-							printf("broadcasting\n\n");
-						}
 						else if (!strcmp(cldata, "quit")) {
 							breakout = 0;
+						}
+						else if (strcmp(cldata, "broadcast") != 0 && !strcmp(strtok(cldata, search), "broadcast")) {
+							strcat(bmsg, strtok(NULL, end));
+
+							for (j=0; j<=clientmax; j++) {			
+								if (FD_ISSET(j, &lisocks)) {
+									if (j != s && j != STDIN) {
+										if ((status = send(j, &bmsg, sizeof(bmsg), 0)) <= 0) {
+											perror("Send");
+										}
+									}
+								}
+							}
+							printf("Broadcast Complete\n\n");
+							memset(bmsg, '\0', sizeof(bmsg[0]) * 100);
+							bmsg[0] = broad[0];
+						}
+						else if (!strcmp(cldata, "broadcast") != 0) {
+							printf("Must broadcast some message\n\n");
 						}
 						else {
 							printf("%s is not a valid command\n\n", cldata);
 						}
-						printf("Please enter a command: ");
+						if (breakout) printf("Please enter a command: ");
 						memset(cldata, '\0', 100);
 					}
 				}
 				else {
-					if ((dalen = recv(i, cldata, sizeof(cldata), 0)) <= 0) {
-						timestamps[j][0] = 0;
+					if ((status = recv(i, cldata, sizeof(cldata), 0)) <= 0) {
+						close(i);
+                        			FD_CLR(i, &lisocks);
 					}
 					else {
-						if (cldata[0] == '0') {
+						if (cldata[0] == welcome[0]) {
 							user = strtok(cldata, search)+2;
 							pass = strtok(NULL, search);
 
-							printf("%s--%s\n", user, pass);
-
 							for (j=0; j<USERS; j++) {
-								printf("%s %s %s %s %d\n", auth[j][0], user, auth[j][1], pass, !strcmp(auth[j][0], user));
 								if (!strcmp(auth[j][0], user) && !strcmp(auth[j][1], pass)) {
-									timestamps[j][0] = 1;
+									timestamps[j][0] += 1;
 									timestamps[j][1] = time(NULL);
-									printf("Logged and timing %lld--%lld\n", timestamps[j][0], timestamps[j][1]);
+									timestamps[j][2] = time(NULL);
 									present = 1;
+
+									if ((status = send(i, &input, sizeof(input), 0)) <= 0) {
+										perror("Send");
+									}
 									break;
 								}
 							}
 
 							if (present == 0) {
 								if (cldata[1] >= '2') {
-									if ((status = send(clsock, &lock, sizeof(lock), 0)) <= 0) {
+									if ((status = send(i, &lock, sizeof(lock), 0)) <= 0) {
 										perror("Send");
 									}
 								}
 								else {
-									if ((status = send(clsock, &incorrect, sizeof(incorrect), 0)) <= 0) {
+									if ((status = send(i, &incorrect, sizeof(incorrect), 0)) <= 0) {
 										perror("Send");
 									}
 								}
 							}
 							else {
 								present = 0;
+							}
+						}
+						else if (cldata[0] == input[0]) {
+							if ((status = send(i, allauths, 1000*sizeof(char), 0)) <= 0) {
+								perror("Send");
+							}
+						}
+						else if (cldata[0] == disc[0]) {
+							user = strtok(cldata, search)+3;
+							pass = strtok(NULL, search);
+							for (j=0; j<USERS; j++) {
+								if (!strcmp(auth[j][0], user) && !strcmp(auth[j][1], pass)) {
+									timestamps[j][0]--;
+									break;
+								}
 							}
 						}
 					}
